d047dee79080ce26f1b82d056ad96d2ad34ff89c
Sunny Gupta <vknow360@gmail.com>
2025-12-21 21:22:13 +0530

    21/12/2024

---
+++ Leetcode/delete-columns-to-make-sorted-ii.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int n = strs.size();
+        int m = strs[0].size();
+        int ans = 0;
+
+        vector<bool> sorted(n - 1, false);
+
+        for (int col = 0; col < m; col++) {
+            bool bad = false;
+
+            for (int row = 0; row < n - 1; row++) {
+                if (!sorted[row] && strs[row][col] > strs[row + 1][col]) {
+                    bad = true;
+                    break;
+                }
+            }
+
+            if (bad) {
+                ans++;
+                continue;
+            }
+
+            for (int row = 0; row < n - 1; row++) {
+                if (!sorted[row] && strs[row][col] < strs[row + 1][col]) {
+                    sorted[row] = true;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
