2b08baec5980d7140d6847c8a31eb6759ed9769a
Sunny Gupta <vknow360@gmail.com>
2026-01-03 18:27:34 +0530

    3/1/2026

---
+++ GeeksForGeeks/flattening-a-linked-list.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    Node* merge(Node* a, Node* b) {
+        if (!a) return b;
+        if (!b) return a;
+
+        Node* result;
+        if (a->data < b->data) {
+            result = a;
+            result->bottom = merge(a->bottom, b);
+        } else {
+            result = b;
+            result->bottom = merge(a, b->bottom);
+        }
+
+        result->next = NULL;
+        return result;
+    }
+
+    Node* flatten(Node* root) {
+        if (!root || !root->next)
+            return root;
+
+        root->next = flatten(root->next);
+        root = merge(root, root->next);
+
+        return root;
+    }
+};
