f948a8cb48003bacb64eca55c6d382a520e868bf
Sunny Gupta <vknow360@gmail.com>
2026-01-05 15:07:36 +0530

    5/1/2026

---
+++ GeeksForGeeks/max-sum-subarray-of-size-k.cpp
@@ -0,0 +1,18 @@
+class Solution {
+  public:
+    int maxSubarraySum(vector<int>& arr, int k) {
+        // code here
+        int n = arr.size();
+        int sum = 0;
+        for(int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+        int ans = sum; 
+        for(int i = k; i < n; i++) {
+            sum -= arr[i-k];
+            sum += arr[i];
+            ans = max(ans, sum);
+        }
+        return ans;
+    }
+};
