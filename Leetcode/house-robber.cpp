dbcb03b364c085916cb2c66f10cbc9944899cac0
Sunny Gupta <vknow360@gmail.com>
2026-01-10 17:01:41 +0530

    10/1/2026

--- Leetcode/house-robber.cpp
+++ Leetcode/house-robber.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    vector<int> dp;
-    int solve(vector<int>& nums, int i, int n) {
-        if(i >= n) return 0;
+    int solve(vector<int>& nums) {
+        vector<int> dp(nums.size()+1, 0); 
+        
+        dp[0] = 0;
+        dp[1] = nums[0];
 
-        if(dp[i] != -1) return dp[i];
-
-        int steal = nums[i] + solve(nums, i+2, n);
-        int skip = solve(nums, i+1, n);
-
-        return dp[i] = max(steal, skip);
+        for(int i = 2; i <= nums.size(); i++) {
+            int skip = dp[i-1];
+            int steal = nums[i-1] + dp[i-2];
+            dp[i] = max(skip, steal);
+        }
+        return dp.back();
     }
     int rob(vector<int>& nums) {
-        dp.assign(101, -1);
-
-        return solve(nums, 0, nums.size());
+        return solve(nums);
     }
 };
