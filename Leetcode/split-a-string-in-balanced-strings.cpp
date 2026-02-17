8a520d34ef6fe55f2b596396da0fd4c14c560f51
Sunny Gupta <vknow360@gmail.com>
2026-02-17 16:36:30 +0530

    17/2/2026

---
+++ Leetcode/split-a-string-in-balanced-strings.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int balancedStringSplit(string s) {
+        int n = s.size();
+        int ans = 0;
+
+        int l = 0, r = 0;
+        for(int i = 0; i < n; i++) {
+            if(s[i] == 'L') l++;
+            else r++;
+
+            if(l == r) {
+                ans++;
+                l = r = 0;
+            }
+        }
+        return ans;
+    }
+};
