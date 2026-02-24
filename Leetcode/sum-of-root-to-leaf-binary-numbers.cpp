887038b22a6deb9950bbcd069d6f6fbc07629a8e
Sunny Gupta <vknow360@gmail.com>
2026-02-24 12:33:58 +0530

    24/2/2026

---
+++ Leetcode/sum-of-root-to-leaf-binary-numbers.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int dfs(TreeNode* root, int curr) {
+        if(!root) return 0;
+
+        curr <<= 1;
+        curr += (root->val);
+
+        if(!root->left && !root->right) return curr;
+
+        return dfs(root->left, curr) + dfs(root->right, curr);
+    }
+    int sumRootToLeaf(TreeNode* root) {
+        return dfs(root, 0);
+    }
+};
