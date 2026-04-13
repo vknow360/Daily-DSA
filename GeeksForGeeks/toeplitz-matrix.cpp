99d8773ed77f3e08217994e45621c27499b0176b
Sunny Gupta <vknow360@gmail.com>
2026-04-13 18:55:54 +0530

    13/4/2026
    Implement isToeplitz function to check Toeplitz matrix.

---
+++ GeeksForGeeks/toeplitz-matrix.cpp
@@ -0,0 +1,15 @@
+class Solution {
+  public:
+    bool isToeplitz(vector<vector<int>>& mat) {
+        // code here
+        int n = mat.size();
+        int m = mat[0].size();
+        
+        for(int i = 0; i < n-1; i++) {
+            for(int j = 0; j < m-1; j++) {
+                if(mat[i][j] != mat[i+1][j+1]) return false;
+            }
+        }
+        return true;
+    }
+};
